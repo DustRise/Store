63c27e479002bed40fe19484ec4e402a860707c1
SivaPandeti <psivah@yahoo.com>
2013-04-02 01:45:43 -0400

    Initial Commit

---
+++ Store/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Store
+//
+//  Created by span on 4/2/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoreTests/StoreTests.h
@@ -0,0 +1,13 @@
+//
+//  StoreTests.h
+//  StoreTests
+//
+//  Created by span on 4/2/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StoreTests : SenTestCase
+
+@end
